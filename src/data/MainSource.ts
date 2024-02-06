b988322db9ec2aa5405873e4fa00dee7e0028096
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2024-02-06 22:15:58 +0100

    Initial commit

---
+++ src/data/MainSource.ts
@@ -0,0 +1,15 @@
+import semver from 'semver';
+
+import DocsSource from './DocsSource';
+
+const branchBlacklist = new Set(['docs']);
+export default new DocsSource({
+	id: 'main',
+	name: 'Main library',
+	global: 'Jellyfin',
+	repo: 'GreepTheSheep/node-jellyfin-sdk',
+	defaultTag: 'main',
+	branchFilter: (branch: string) =>
+		!branchBlacklist.has(branch) && !branch.startsWith('dependabot/') && !branch.startsWith('depfu/'),
+	tagFilter: (tag: string) => semver.gte(tag.replace(/^v/, ''), '3.0.0'),
+});
---
+++ src/router.ts
@@ -0,0 +1,17 @@
+import { createRouter, createWebHashHistory } from 'vue-router';
+import routes from 'virtual:generated-pages';
+
+export default createRouter({
+	history: createWebHashHistory(),
+	routes: [
+		...routes,
+		{
+			path: "/discord-bot",
+			name: "Discord Bot",
+			redirect: "",
+			beforeEnter() {
+				window.location.href = "https://discord.com/oauth2/authorize?client_id=810191349214871583&permissions=397888841808&scope=bot%20applications.commands";
+			}
+		}
+	],
+});
