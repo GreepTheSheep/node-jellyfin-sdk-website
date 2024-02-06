63b87be717a83a8d014c009ee4fdfae901a74985
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2024-02-06 22:23:15 +0100

    Refilter

--- src/router.ts
+++ src/router.ts
@@ -5,13 +5,5 @@ export default createRouter({
 	history: createWebHashHistory(),
 	routes: [
 		...routes,
-		{
-			path: "/discord-bot",
-			name: "Discord Bot",
-			redirect: "",
-			beforeEnter() {
-				window.location.href = "https://discord.com/oauth2/authorize?client_id=810191349214871583&permissions=397888841808&scope=bot%20applications.commands";
-			}
-		}
 	],
 });
